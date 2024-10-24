f4870b32ad29a927fccd235f00abcd7072e98aa6
Luan-Marchiotto <luanmarchiotto@gmail.com>
2024-10-24 19:56:52 -0300

    Problems

--- Java/SomaSimples_1003.java
+++ Java/SomaSimples_1003.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 
-public class SomaSimples_1003 {
+public class SomaSimples_1003{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        int soma = a + b;
-
-        System.out.println(soma);
-
-        sc.close();
+        try (Scanner sc = new Scanner(System.in)){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+    
+            int soma = a + b;
+    
+            System.out.println(soma);
+        }
     }
 }
