945141c3c99d92a8b98cec18de201ba44b61cab8
Luan-Marchiotto <luanmarchiotto@gmail.com>
2024-10-24 19:46:07 -0300

    Problems

---
+++ Java/SomaSimples_1003.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SomaSimples_1003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int soma = a + b;
+
+        System.out.println(soma);
+    }
+}
