c6d49959b12cac1a4850ccc9a4901280c89128cb
Luan-Marchiotto <luanmarchiotto@gmail.com>
2024-10-24 19:26:04 -0300

    Problems

---
+++ Java/src/ajskajs.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class ajskajs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("SDKJNSDKJSNDKJ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int soma = a + b;
+
+        System.out.println(soma);
+
+        sc.close();
+    }
+    
+}
