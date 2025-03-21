c2f3158ad408d7a7f89c16fca53b575ef7c47b7a
Luan Marchiotto Silva <luanmarchiotto@gmail.com>
2025-03-21 11:23:07 -0300

    Problems

---
+++ Sql/2994_QuantoGanhaUmMedico.sql
@@ -0,0 +1,6 @@
+SELECT doctors.name, ROUND(SUM(attendances.hours * 150) + SUM(attendances.hours * 150 * (work_shifts.bonus / 100)), 1) AS salary
+FROM doctors
+INNER JOIN attendances ON doctors.id = attendances.id_doctor
+INNER JOIN work_shifts ON attendances.id_work_shift = work_shifts.id
+GROUP BY doctors.name
+ORDER BY salary DESC;
