b81fd0542efd1866ef40e8d3dbbcb559e6489e51
Luan Marchiotto Silva <luanmarchiotto@gmail.com>
2025-03-28 14:27:24 -0300

    Problems

---
+++ Java/CalculoSimples_1010.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class CalculoSimples_1010  {
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)){
+
+            String valores1 = sc.nextLine();
+            String valores2 = sc.nextLine();
+
+
+            String[] valores1Array = valores1.split(" ");
+            String[] valores2Array = valores2.split(" ");
+
+            int a_1 = Integer.parseInt(valores1Array[0]);
+            int b_1 = Integer.parseInt(valores1Array[1]);
+            double c_1 = Double.parseDouble(valores1Array[2]);
+
+            // Convertendo os valores da segunda linha
+            int a_2 = Integer.parseInt(valores2Array[0]);
+            int b_2 = Integer.parseInt(valores2Array[1]);
+            double c_2 = Double.parseDouble(valores2Array[2]);
+
+            double total1 = b_1 * c_1;
+            double total2 = b_2 * c_2;
+
+            double geral = total1 + total2;
+
+            System.out.printf("VALOR A PAGAR: R$ %.2f\n", geral);
+
+        } 
+    }
+}
--- Java/ExtremamenteBasico_1001.java
+++ Java/ExtremamenteBasico_1001.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-public class SomaSimples_1003{
+public class ExtremamenteBasico_1001 {
     public static void main(String[] args) {
         
-        try (Scanner sc = new Scanner(System.in)){
+        try (Scanner sc = new Scanner(System.in)) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-    
+
             int soma = a + b;
-    
-            System.out.printf("SOMA = " + soma + "\n");
+                
+            System.out.println(soma);
         }
     }
 }
