2c06ca8fd5a8867cc229bf325ab9238002511165
Luan Marchiotto Silva <luanmarchiotto@gmail.com>
2025-04-03 10:03:19 -0300

    Problems

---
+++ Python/2234_CachorrosQuentes.py
@@ -0,0 +1,3 @@
+quantidade, pessoas = map(float,input().split())
+
+print(f"{quantidade / pessoas:.2f}")
---
+++ Python/2803_EstadosDoNorte.py
@@ -0,0 +1,16 @@
+estado = input()
+
+regiao = {
+    'para': "Regiao Norte",
+    'acre': "Regiao Norte",
+    'roraima' : "Regiao Norte",
+    'amapa' : "Regiao Norte",
+    'amazonas' : "Regiao Norte",
+    'rondonia' : "Regiao Norte",
+    'tocantins' : "Regiao Norte",
+}
+
+if estado in regiao:
+    print(regiao[estado])
+else:
+    print("Outra regiao")
