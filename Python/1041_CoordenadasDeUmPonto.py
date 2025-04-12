33f6047eaf871e2c83a3cc0ed55ce9899ea8a1fc
Luan Marchiotto Silva <luanmarchiotto@gmail.com>
2025-04-12 20:25:27 -0300

    Problems

--- Python/1041_CoordenadasDeUmPonto.py
+++ Python/1041_CoordenadasDeUmPonto.py
@@ -1,18 +1,14 @@
 x, y = map(float, input().split())
 
-# Verifica se o ponto está na origem
 if x == 0 and y == 0:
     print("Origem")
 
-# Verifica se o ponto está sobre o eixo Y
 elif x == 0:
     print("Eixo Y")
 
-# Verifica se o ponto está sobre o eixo X
 elif y == 0:
     print("Eixo X")
 
-# Verifica os quadrantes
 elif x > 0 and y > 0:
     print("Q1")
 
