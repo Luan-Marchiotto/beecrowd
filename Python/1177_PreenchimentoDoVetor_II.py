4c686eeb5473e100124d59028f2605eaaad86f0f
Luan Marchiotto Silva <luanmarchiotto@gmail.com>
2025-04-13 22:16:02 -0300

    Problems

--- Python/1177_PreenchimentoDoVetor_II.py
+++ Python/1177_PreenchimentoDoVetor_II.py
@@ -3,7 +3,7 @@ T = int(input())
 N = []
 
 for i in range(1000):
-    N.append(i % T) # Adiciona somente os numeros dos restos de 0 até T-1
+    N.append(i % T)
 
 for i in range(1000):
     print(f'N[{i}] = {N[i]}')
