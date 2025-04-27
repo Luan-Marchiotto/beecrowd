46ab3a77e15cb0d857ffe3e42ca112b0e6af090a
Luan Marchiotto Silva <luanmarchiotto@gmail.com>
2025-04-26 21:24:37 -0300

    Poblems

--- Python/3312_ImberbeMatematico.py
+++ Python/3312_ImberbeMatematico.py
@@ -1,6 +1,7 @@
 import math
 
 def is_prime(n):
+    
     if n < 2:
         return False
     for i in range(2, int(math.sqrt(n)) + 1):
@@ -14,4 +15,5 @@ numeros = list(map(int, input().split()))
 primos = [num for num in numeros if is_prime(num)]
     
 for prime in primos:
+    
     print(f"{prime}! = {math.factorial(prime)}")
