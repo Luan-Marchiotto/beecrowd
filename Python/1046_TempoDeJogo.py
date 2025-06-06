c6998996ba781226a42d73b1e8c4142fbd03d43b
Luan Marchiotto Silva <luanmarchiotto@gmail.com>
2025-06-06 14:41:07 -0300

    Problems

--- Python/1046_TempoDeJogo.py
+++ Python/1046_TempoDeJogo.py
@@ -2,7 +2,9 @@ inicial, final = map(int,input().split())
 
 if inicial > final:
     print(f"O JOGO DUROU {24 - inicial + final} HORA(S)")
+    
 elif final > inicial:
     print(f"O JOGO DUROU {final - inicial} HORA(S)")
+    
 else:
     print(f"O JOGO DUROU 24 HORA(S)")
